cfb59a09e3141e9c4cb0f401114d71b107f8a6cc
Saurabh Kumar <saurabh09.ind@gmail.com>
2023-07-11 03:57:06 +0530

    script.js

---
+++ projects/circular-navigation/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('.menu');
+let menuToggle = document.querySelector('.toggle');
+
+menuToggle.onclick = function() {
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+}
