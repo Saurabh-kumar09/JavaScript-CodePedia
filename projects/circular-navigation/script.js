b3641151c3ae078ddfa609760f25ae838df2df45
Saurabh Kumar <saurabh09.ind@gmail.com>
2023-07-11 04:51:50 +0530

    js update

--- projects/circular-navigation/script.js
+++ projects/circular-navigation/script.js
@@ -5,3 +5,14 @@ menuToggle.onclick = function() {
     menu.classList.toggle('active')
     menuToggle.classList.toggle('active')
 }
+
+const list = document.querySelector("li");
+
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active')
+}
+
+list.forEach((item) =>
+    item.addeventListener('click', activeLink))
